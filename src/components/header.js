2f625d53bf665f29d05cc69ee5d5ae353d7c2969
Sergio Carrasco <sergio.carrasco.a@gmail.com>
2021-10-03 23:07:13 -0700

    cabecera

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ export default () => (
     <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center">
         <div className="flex-1">
             <h1 className="font-bold text-gray-900 text-4xl">PWA Portfolio</h1>
-            <p className="text-xl">GatsbyJS + TailwindCSS + GraphQL</p>
+            <p className="text-xl">ReactJS/GatsbyJS + TailwindCSS + GraphQL</p>
         </div>
         <img src={logo} alt="joven sentado en un escritorio junto a una pc" style={{width: "40%"}} ></img>
     </div>
